8c971380f979408ac72be348e457b92955fdc02c
Julian Schoepf <ferlojulian@hotmail.com>
2022-01-13 22:25:27 +0100

    10 image links

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 import array from 'array';
-// download html with fetch then parse with domparser api
-// nth child
-// To get the first n elements of an array, use:
-// const slicedArray = array.slice(0, n);
-// const response = await fetch(
-//   'https://memegen-link-examples-upleveled.netlify.app/',
-// );
-// const body = await response.text();
-// console.log(body);
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
@@ -25,8 +16,6 @@ const main = async () => {
       return $(this).find('img').attr('src');
     })
     .toArray();
-  // console.info({ toString: 5 }['toString']); // 5
-  // console.info({}['toString']);
 
   console.log(memes.slice(0, 10));
 };
